ffca8217ecd2f83883323b4b5e05d5ea7f9ffa86
Lineker Milani <lineker.milani@hotmail.com>
2021-09-09 12:10:37 -0300

    Exercicio de Fixação

---
+++ src/application/Fixacao.java
@@ -0,0 +1,45 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.exceptions.AccountException;
+
+import model.entities.Account;
+
+public class Fixacao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			System.out.println("Enter account data");
+			System.out.print("Number: ");
+			int number = sc.nextInt();
+			System.out.print("Holder: ");
+			sc.nextLine();
+			String holder = sc.nextLine();
+			System.out.print("Initial balance: ");
+			double balance = sc.nextDouble();
+			System.out.print("Withdraw Limit: ");
+			double withdrawLimite = sc.nextDouble();
+			Account account = new Account(number, holder, balance, withdrawLimite);
+			
+			System.out.println();
+			System.out.print("Enter amount for withdraw: ");
+			double amount = sc.nextDouble();
+			account.withdraw(amount);
+			System.out.println("New balance: " + String.format("%.2f", account.getBalance()));
+		} catch (AccountException e) {
+			System.out.println("Withdraw error: " + e.getMessage());
+		} catch (RuntimeException e) {
+			System.out.println("Unexpected error");
+		}
+		
+		sc.close();
+	}
+
+}
